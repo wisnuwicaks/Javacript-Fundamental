f31e98dbb2f3b6301f762ed8c84200cf4d1f97a9
wisnuwicaks <wisnucaks@gmail.com>
2020-03-26 12:44:19 +0700

    finish bonus

--- Online-Zoom/OnlineDay3.js
+++ Online-Zoom/OnlineDay3.js
@@ -1,19 +1,20 @@
-
-// traingle =(baris)=>{
-//     arrUtama =[]
-//     for(i=1;i<=baris;i++){
-//         arrIsi=[]
-//         for(a=1;a<=i;a++){
-//         arrIsi.push(a)
-//         }
-//         arrUtama = [...arrUtama,[...arrIsi]]
+//========Triangle===========//
+const traingle =(baris)=>{
+    arrUtama =[]
+    for(i=1;i<=baris;i++){
+        arrIsi=[]
+        for(a=1;a<=i;a++){
+        arrIsi.push(a)
+        }
+        arrUtama = [...arrUtama,[...arrIsi]]
         
-//     }
-//     return arrUtama
-// }
-// console.log(traingle(6))
+    }
+    return arrUtama
+}
+console.log(traingle(6))
 
-// traingle =(baris)=>{
+////========SegitigaGanjil===========//
+// const traingleGanjil =(baris)=>{
 //     arrUtama =[]
 //     a=1
 //     for(i=1;i<=baris;i++){
@@ -28,37 +29,40 @@
 //     }
 //     return arrUtama
 // }
-// console.log(traingle(15))
+// console.log(traingleGanjil(15))
 
-const triangleFibo = (baris) =>{
-arr = [1,1,2]
-for(i=3;i<10000;i++){ //membuat baris fibo
-arr[i] = arr[i-1]+arr[i-2]
-}
-arrHasil = []
-for(i=1;i<=baris;i++){
-arrHasil.push(arr.splice(0,i))
-}
-return arrHasil
-}
-console.log(triangleFibo(5))
+// //========Array Fibonacci===========//
+// const triangleFibo = (baris) =>{
+// arr = [1,1,2]
+// for(i=3;i<10000;i++){ //membuat baris fibo
+// arr[i] = arr[i-1]+arr[i-2]
+// }
+// arrHasil = []
+// for(i=1;i<=baris;i++){
+// arrHasil.push(arr.splice(0,i))
+// }
+// return arrHasil
+// }
+// console.log(triangleFibo(5))
 
-// [1]
-// [1,1]
-// [1,2,1]
-// [1,3,3,1]
 
+////========Segitga Pascal===========//
 // const trianglePascal = (baris) =>{
 // arr1 =  [1]
 // arr2 = [1,1]
 // arrHasil = [[...arr1],[...arr2]]
 // arrJumlah = []
-// for(i=1;i<arrHasil[baris].length;i++){
-// arrJumlah[i-1] = arr2[i]+arr2[i-1]
+// iterasi = 1
+//     while (iterasi <=baris-2){
+//         for(i=1;i<arrHasil[arrHasil.length-1].length;i++){
+//         arrJumlah[i-1] = arrHasil[arrHasil.length-1][i]+arrHasil[arrHasil.length-1][i-1]
+//         }
+//         arrJumlah.unshift(1)
+//         arrJumlah.push(1)
+//         arrHasil.push(arrJumlah)
+//         arrJumlah = []
+//         iterasi ++
+//     }
+//     return arrHasil
 // }
-// arrJumlah.unshift(1)
-// arrJumlah.push(1)
-// arrHasil.push(arrJumlah)
-// return arrHasil
-// }
-// console.log(trianglePascal(7))
+// console.log(trianglePascal(5))
